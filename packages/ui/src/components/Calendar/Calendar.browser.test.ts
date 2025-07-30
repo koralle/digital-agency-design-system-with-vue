46f320c04ff48e1234774f49cbb044a2763aac15
koralle <koralle@koralle-macbook-air.local>
2025-07-30 23:29:24 +0900

    feat: update `delete` and `today`

--- packages/ui/src/components/Calendar/Calendar.browser.test.ts
+++ packages/ui/src/components/Calendar/Calendar.browser.test.ts
@@ -6,12 +6,31 @@ import { render } from '../../test/render';
 const composedStories = composeStories(CalendarStories);
 
 describe('calendar', () => {
-  const { Default } = composedStories;
+  describe('コンポーネントが正しくレンダリングされていること', async () => {
+    const { Default } = composedStories;
 
-  test('コンポーネントが正しくレンダリングされていること', async () => {
-    render(Default);
+    test('年を切り替えるセレクトボックスが表示されていること', async () => {
+      render(Default);
 
-    const heading = page.getByRole('heading', { level: 1, name: /むぎちゃ/i });
-    await expect.element(heading).toBeVisible();
+      const yearSelect = page.getByRole('combobox', { name: '年', exact: true });
+      await expect.element(yearSelect).toBeVisible();
+      await expect.element(yearSelect).toBeEnabled();
+    });
+
+    test('前の月に切り替えるボタンが表示されていること', async () => {
+      render(Default);
+
+      const button = page.getByRole('button').filter({ has: page.getByLabelText('前の月') });
+      await expect.element(button).toBeVisible();
+      await expect.element(button).toBeEnabled();
+    });
+
+    test('次の月に切り替えるボタンが表示されていること', async () => {
+      render(Default);
+
+      const button = page.getByRole('button').filter({ has: page.getByLabelText('次の月') });
+      await expect.element(button).toBeVisible();
+      await expect.element(button).toBeEnabled();
+    });
   });
 });
