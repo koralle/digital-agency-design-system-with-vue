4c4b25c18e423e177f4a950e9cd9383d4a58f786
koralle <koralle@koralle-macbook-air.local>
2025-07-31 21:06:59 +0900

    feat: separate header and footer from `Calendar.vue`

---
+++ packages/ui/src/components/Calendar/CalendarFooter.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex self-stretch justify-between gap-4 p-4">
+    <button
+      :class="[
+        'relative',
+        'text-[1em]',
+        'min-w-[5rem] min-h-[2.25rem]',
+        'rounded-[6px]',
+        'px-3 py-0.5',
+        'font-700',
+        'text-primitive-blue-900',
+        'tracking-[0.04em]',
+        'cursor-pointer',
+        'underline underline-offset-[calc(3/16*1rem)]',
+        'hover:bg-primitive-blue-50',
+        'hover:text-primitive-blue-1000',
+        'hover:decoration-[calc(3/16*1rem)]',
+        'active:bg-primitive-blue-100',
+        'active:text-primitive-blue-1100',
+        'focus-visible:outline-2 focus-visible:outline-primitive-lime-1200 focus-visible:outline-offset-2',
+        'aria-disabled:bg-transparent',
+        'aria-disabled:pointer-events-none',
+        'not-motion-reduce:transition not-motion-reduce:duration-300',
+        'motion-reduce:transition-none',
+        'after:absolute after:inset-x-0 after:-inset-y-full after:m-auto after:h-[44px]'
+      ]"
+    >
+      削除
+    </button>
+    <button
+      :class="[
+        'relative',
+        'text-[1em]',
+        'min-w-[5rem] min-h-[2.25rem]',
+        'rounded-[6px]',
+        'px-3 py-0.5',
+        'font-700',
+        'text-primitive-blue-900',
+        'underline-offset-[calc(3/16*1rem)]',
+        'tracking-[0.04em]',
+        'cursor-pointer',
+        'border',
+        'border-current',
+        'bg-neutral-white',
+        'hover:bg-primitive-blue-200',
+        'hover:text-primitive-blue-1000',
+        'hover:underline',
+        'active:bg-primitive-blue-300',
+        'active:text-primitive-blue-1200',
+        'active:underline',
+        'focus-visible:outline-2 focus-visible:outline-primitive-lime-1200 focus-visible:outline-offset-2',
+        'aria-disabled:bg-white',
+        'aria-disabled:text-neutral-solid-gray-300',
+        'aria-disabled:pointer-events-none',
+        'not-motion-reduce:transition not-motion-reduce:duration-300',
+        'motion-reduce:transition-none',
+        'after:absolute after:inset-x-0 after:-inset-y-full after:m-auto after:h-[44px]'
+      ]"
+    >
+      今日
+    </button>
+  </div>
+</template>
