6f3dbd03a321e7107c3700443acfbb31d721e7cc
koralle <koralle@koralle-macbook-air.local>
2025-08-01 22:00:53 +0900

    wip

--- packages/ui/src/components/Calendar/CalendarGrid.vue
+++ packages/ui/src/components/Calendar/CalendarGrid.vue
@@ -1,12 +1,44 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, nextTick, ref, useTemplateRef, watchEffect } from 'vue';
+import type { ShallowRef } from 'vue';
 import { useCalendarContext } from './calendar-context';
-import { getMonthDays, getWeekdays } from './calendar-utils';
+import { getMonthDays, getWeekdays, isSameMonth } from './calendar-utils';
 import CalendarCell from './CalendarCell.vue';
 
 const { calendarDate } = useCalendarContext();
 
 const days = computed(() => getMonthDays(calendarDate.value));
+
+const buildRefKey = (date: Date): `${number}-${number}-${number}` =>
+  `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
+const daysMap = ref(new Map<string, Readonly<ShallowRef<typeof CalendarCell | null>>>());
+
+watchEffect(() => {
+  for (const date of days.value.flat().filter((date) => isSameMonth(date, calendarDate.value))) {
+    const calendarCellRefKey = buildRefKey(date);
+    const calendarRefCellRef = useTemplateRef<typeof CalendarCell | null>(calendarCellRefKey);
+
+    daysMap.value.set(calendarCellRefKey, calendarRefCellRef);
+  }
+});
+
+const handleFocusNext = async (date: Date) => {
+  if (!isSameMonth(date, calendarDate.value)) {
+    await nextTick();
+  }
+
+  const nextKey = buildRefKey(date);
+  const buttonRef = daysMap.value.get(nextKey);
+
+  if (typeof buttonRef === 'undefined') {
+    return;
+  }
+
+  if (buttonRef.value === null) {
+    return;
+  }
+};
 </script>
 
 <template>
@@ -38,10 +70,12 @@ const days = computed(() => getMonthDays(calendarDate.value));
         :class="['grid grid-cols-[repeat(7,calc(48/16*1rem))]', '!h-[calc(48/16*1rem)]']"
       >
         <CalendarCell
+          :data-date="buildRefKey(cell)"
+          :ref="buildRefKey(cell)"
           v-for="(cell, colIndex) of cells"
           :key="`${rowIndex}-${colIndex}`"
           :date="cell"
-          @focus-next="(date) => console.log(date)"
+          @focus-next="handleFocusNext"
         />
       </tr>
     </tbody>
